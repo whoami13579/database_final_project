4bc27dc5626947b8283fc2d703e7efd21ce98eb9
whoami13579 <liangforcoding@gmail.com>
2024-05-16 23:06:48 +0800

    add add_journal_artical function in views.py

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import current_user, login_required
-from .models import Role, Teacher
+from .models import Role, Teacher, JournalArtical
 from . import db
 from datetime import datetime
 
@@ -21,3 +21,31 @@ def role(role_id):
 @views.route("/teacher/<teacher_id>")
 def teacher(teacher_id):
     return render_template("teacher.html", user=current_user, teacher=Teacher.query.filter_by(teacher_id=teacher_id).first())
+
+
+@views.route("/add-journal-artical/", methods=["GET", "POST"])
+@login_required
+def add_journal_artical():
+    if request.method == "POST":
+        course_name = request.form.get("course_name")
+        journal_name = request.form.get("journal_name")
+        collaborators = request.form.get("collaborators")
+        page_number_of_the_journal = request.form.get("page_number_of_the_journal")
+        page_number_of_the_journal = int(page_number_of_the_journal)
+        indexed_website = request.form.get("indexed_website")
+        indexed_time = request.form.get("indexed_time")
+        date_format = "%Y-%m-%d"
+        indexed_time = datetime.strptime(indexed_time, date_format)
+        
+        if JournalArtical.query.filter_by(course_name=course_name).first():
+            flash("Journal Artical already exists", category="error")
+        else:
+            journalArtical = JournalArtical(course_name, journal_name, collaborators, page_number_of_the_journal, indexed_website, indexed_time)
+            current_user.journal_articals.append(journalArtical)
+            journalArtical.teachers.append(current_user)
+            db.session.add(journalArtical)
+            db.session.commit()
+
+            flash("Add Journal Artical", category="success")
+            return redirect(url_for("views.teacher", teacher_id=current_user.get_id()))
+    return render_template("add_journal_artical.html", user=current_user)
